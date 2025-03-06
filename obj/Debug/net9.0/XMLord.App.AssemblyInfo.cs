b7d53a66d94d12554ded7c98e0e97cf6850c5805
Dio-Kyrie <diokyrie.oppressor643@passinbox.com>
2025-03-07 00:32:28 +0200

    Clearing up and testing

--- obj/Debug/net9.0/XMLord.App.AssemblyInfo.cs
+++ obj/Debug/net9.0/XMLord.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XMLord.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71974f7dea781a0ea80cd16ee19827010d1d0f13")]
 [assembly: System.Reflection.AssemblyProductAttribute("XMLord.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XMLord.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
