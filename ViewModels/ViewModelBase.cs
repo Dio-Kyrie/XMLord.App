71974f7dea781a0ea80cd16ee19827010d1d0f13
Dio-Kyrie <diokyrie.oppressor643@passinbox.com>
2025-03-06 23:46:31 +0200

    Initial commit of MVVM project

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace XMLord.App.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace XMLord.App.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
